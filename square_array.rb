792f44024e29884ce7aac4049e004b3b90e750ec
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-13 20:30:27 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,7 @@
 def square_array(array)
-   new_array = []
-  array.each{|a| new_array.push(a*a)}
-  return new_array
+  
+squared = []
+  array.each { |element| squared << element ** 2 }
+  squared
 
 end
